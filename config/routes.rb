97fbdb6755e6f2f6f3e639a63c79cc9358fb1d74
Chase Dunagan <cdunagan@utexas.edu>
2016-11-29 13:08:40 -0700

    add merchant relationship endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -32,9 +32,23 @@ Rails.application.routes.draw do
         get "/find", to: "find#show"
         get "/random", to: "random#show"
       end
-      resources :transactions, only: [:index, :show]
-      resources :customers, only: [:index, :show]
-      resources :merchants, only: [:index, :show]
+      resources :transactions, only: [:index, :show] do
+        scope module: 'transactions' do
+          get "/invoice", to: "invoice#show"
+        end
+      end
+      resources :customers, only: [:index, :show] do
+        scope module: 'customers' do
+          get "/invoices", to: "invoices#index"
+          get "/transactions", to: "transactions#index"
+        end
+      end
+      resources :merchants, only: [:index, :show] do
+        scope module: 'merchants' do
+          get "/items", to: "items#index"
+          get "/invoices", to: "invoices#index"
+        end
+      end
       resources :invoices, only: [:index, :show]
       resources :items, only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
