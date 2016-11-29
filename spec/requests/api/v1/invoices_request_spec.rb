072c1a0a4638b7adc289333dcfa1eb140a42aa40
Chase Dunagan <cdunagan@utexas.edu>
2016-11-28 21:53:49 -0700

    add index and show functionality to invoices

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -5,7 +5,6 @@ describe "invoices endpoints" do
     it "returns a list of all invoices" do
 
       create_list(:invoice, 3)
-      byebug
 
       get "/api/v1/invoices"
 
@@ -13,6 +12,20 @@ describe "invoices endpoints" do
 
       expect(response).to be_success
       expect(invoices.count).to eq(3)
+    end
+  end
+
+  context "GET /invoices/:id" do
+    it "returns a specific invoice" do
+      invoice_1 = create(:invoice, status: "test")
+      invoice_2 = create(:invoice, status: "testing")
+
+      get "/api/v1/invoices/#{invoice_2.id}"
+
+      invoice = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoice["status"]).to eq("testing")
     end
   end
 end
