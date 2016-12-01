c7b940108f0a6dfb532b85c8db7e828821b9f8f2
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-01 07:08:42 -0700

    favorites work, but need refactoring

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,10 +3,19 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
 
   def total_revenue
     invoices.joins(:invoice_items, :transactions)
       .merge(Transaction.successful)
       .sum("invoice_items.unit_price*invoice_items.quantity")
+  end
+
+  def self.favorite_merchant(customer_id)
+    joins(:invoices, :transactions)
+    .where(invoices: {customer_id: customer_id}, transactions: {result: 'success'})
+    .group('merchants.id').count
+    .max_by { |k,v| v }.first
   end
 end
